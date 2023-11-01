9c570079b5ebf58bebff838e4973855007f52991
TUJungMin <paulkjm92@gmail.com>
2023-11-01 19:50:28 +0900

    최종 완성(2020180002_곽정민)

--- play_mode.py
+++ play_mode.py
@@ -1,11 +1,26 @@
 from pico2d import *
 import game_framework
-objects = [[], [], []]
 import game_world
 from grass import Grass
 from bird import Bird
 
-# bird = None
+grass = None
+birds = []  # birds 리스트를 추가
+
+def init():
+    global grass
+    global birds
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    birds = []
+
+    for i in range(10):
+        bird = Bird()
+        birds.append(bird)
+
+    game_world.add_objects(birds, 1)
 
 def handle_events():
     events = get_events()
@@ -15,34 +30,14 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         else:
-            birds.handle_event(event)
-
-def init():
-    global grass
-    global birds  # birds 리스트를 추가
-
-    grass = Grass()
-    game_world.add_object(grass, 0)
-
-    birds = []  # 새 객체들을 저장할 리스트
-
-    for i in range(10):  # 10마리의 새 객체 생성
-        bird = Bird()
-        birds.append(bird)  # 리스트에 추가
-
-    game_world.add_objects(birds, 1)  # bird 리스트를 game_world에 추가
-
-
+            for bird in birds:
+                bird.handle_event(event)
 
 def finish():
     game_world.clear()
-    pass
-
 
 def update():
     game_world.update()
-    #delay(0.1)
-
 
 def draw():
     clear_canvas()
@@ -54,4 +49,3 @@ def pause():
 
 def resume():
     pass
-
